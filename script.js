88bbe2da144280b77f4fdddb1c4cea49db30f62c
F_ford <76035588+tewarad2020@users.noreply.github.com>
2021-08-05 14:28:22 +0700

    completed

--- script.js
+++ script.js
@@ -4,10 +4,64 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
-// define more constants and variables here
+const search = document.querySelector('#search')
+const reset = document.querySelector('#reset')
+
+author.innerHTML = '630610731 Tewarad Somrad'
+
+let status = true
+let count = 0
 
 btn_toggle.onclick = () => {
-  // your code here
+    if (status) {
+        btn_toggle.innerHTML = 'Show Author'
+        author.innerHTML = 630610731 + count
+        status = false
+    }else {
+        btn_toggle.innerHTML = 'Show Author'
+        author.innerHTML = '630610731 Tewarad Somrad'
+        status = true
+    }
 }
 
-// more codes for Search and Reset buttons here
+const paraG = document.querySelector('#text')
+const alltext = paraG.innerText
+let word = [] 
+
+search.onclick = () => {
+    count = 0
+    const leng = document.querySelector('#length').value
+    paraG.innerHTML = ''
+    for (let i=0;i<alltext.length;i++) {
+        word.push(alltext[i])
+        if (alltext[i] === ' ' || i === alltext.length-1) {
+            if (word.length > leng) {
+                const span = document.createElement('span')
+                for (let j=0;j<word.length-1;j++) {
+                    span.append(word[j])
+                }
+                word = []
+                count++
+                span.style.color = document.querySelector('#color').value
+                paraG.append(span)
+                paraG.append(' ')
+            }
+            else {
+              for (let j=0;j<word.length;j++) {
+                  paraG.append(word[j])
+              }
+              word = []
+            }
+        }
+    }
+}
+
+reset.onclick = () => {
+    count = 0
+    author.innerHTML = '630610731 Tewarad Somrad'
+    status = true
+    document.querySelector('#length').value = 5
+    document.querySelector('#color').value = "#FF0000"
+    paraG.innerHTML = ''
+    paraG.append(alltext)
+}
